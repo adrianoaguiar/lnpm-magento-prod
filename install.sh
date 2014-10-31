7ccbd7b37cd94d7a0c4ff135381db385b5b5c932
Sergey Cherepanov <s@cherepanov.org.ua>
2014-10-31 15:23:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,36 @@
-#!/bin/bash
-TMPDIR=/tmp/magentoenvironmentconfiguration
-GITBIN=/usr/bin/git
-
+#!/usr/bin/env bash
 # Ignore the questions
 export DEBIAN_FRONTEND=noninteractive
+TMPDIR=/tmp/magentoenvironmentconfiguration
 # Update
 # --------------------
 apt-get update
-
-# Install git
+apt-get -y upgrade
+# Mysql
 # --------------------
-apt-get -q -y install git
-
-$GITBIN clone  https://github.com/SergeyCherepanov/magentoenvironmentconfiguration.git $TMPDIR
-
+# Install MySQL quietly
+apt-get -q -y install mysql-server
+# Install nginx + varnish + php-fpm
+apt-get install -y git mysql-client varnish vim nginx php5-fpm php5-cli php5-dev php5-mysql php5-curl php5-gd php5-mcrypt php5-sqlite php5-xmlrpc php5-xsl php5-common
+clone  https://github.com/SergeyCherepanov/magentoenvironmentconfiguration.git $TMPDIR
 cd $TMPDIR
-chmod +x bootstrap.sh
-./bootstrap.sh
-cd -
+
+# Prepare environment config
+# --------------------
+cp ./conf/nginx/sites-available/default /etc/nginx/sites-available/default
+cp ./conf/mysql/my.cnf /etc/mysql/my.cnf
+cp ./conf/php/php.ini /etc/php5/fpm/php.ini
+cp ./conf/varnish/default.vcl /etc/varnish/
+cp ./conf/varnish/varnish.conf /etc/default/varnish
+echo $RANDOM | md5sum | awk '{ print $1 }' > /etc/varnish/secret
+mkdir /var/www
+chown www-data:www-data /var/www
+rm /var/lib/mysql/ibdata1
+rm /var/lib/mysql/ib_logfile0
+rm /var/lib/mysql/ib_logfile1
+service nginx restart
+service php5-fpm restart
+service mysql restart
+service varnish restart
+
 rm -rf $TMPDIR
