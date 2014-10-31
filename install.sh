eca6b75a736af27387f0b860e8d050e61f3352ca
Sergey Cherepanov <s@cherepanov.org.ua>
2014-10-31 13:46:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,13 +10,11 @@ apt-get update
 # Install git
 # --------------------
 apt-get -q -y install git
-
-git --work-tree=$TMPDIR --git-dir=$TMPDIR init
-git --work-tree=$TMPDIR --git-dir=$TMPDIR remote add origin https://github.com/SergeyCherepanov/magentoenvironmentconfiguration.git
-git --work-tree=$TMPDIR --git-dir=$TMPDIR fetch origin master
-git --work-tree=$TMPDIR --git-dir=$TMPDIR checkout -f master
-
 cd $TMPDIR
+
+git clone --quiet https://github.com/SergeyCherepanov/magentoenvironmentconfiguration.git
+git clone --quiet
+
 chmod +x ./bootstrap.sh && ./bootstrap.sh
 cd -
 #rm -rf /tmp/magentoenvironmentconfiguration
