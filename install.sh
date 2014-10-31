7911d617c32180f2bd577d4d3ee1a2b0ece806f5
Sergey Cherepanov <s@cherepanov.org.ua>
2014-10-31 13:29:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+TMPDIR=/tmp/magentoenvironmentconfiguration
 # Ignore the questions
 export DEBIAN_FRONTEND=noninteractive
 # Update
@@ -7,9 +7,14 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 # Install git
 # --------------------
-apt-get -q -y install git
-git clone https://github.com/SergeyCherepanov/magentoenvironmentconfiguration.git /tmp/magentoenvironmentconfiguration
-cd /tmp/magentoenvironmentconfiguration
+apt-get -q -y install unzip
+wget https://github.com/SergeyCherepanov/magentoenvironmentconfiguration.git /tmp/magentoenvironmentconfiguration
+git --work-tree=$TMPDIR --git-dir=$TMPDIR init
+git --work-tree=$TMPDIR --git-dir=$TMPDIR remote add origin https://github.com/SergeyCherepanov/magentoenvironmentconfiguration.git
+git --work-tree=$TMPDIR --git-dir=$TMPDIR fetch origin master
+git --work-tree=$TMPDIR --git-dir=$TMPDIR checkout -f master
+
+cd $TMPDIR
 chmod +x ./bootstrap.sh && ./bootstrap.sh
 cd -
 #rm -rf /tmp/magentoenvironmentconfiguration
